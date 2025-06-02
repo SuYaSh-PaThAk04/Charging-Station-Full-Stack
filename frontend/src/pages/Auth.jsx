b6424bb12302d7b1f9c7ad84275e545ef581191e
Suyash Pathak <suyashp271@gmail.com>
2025-06-03 03:03:59 +0530

    Update Auth.jsx

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -14,7 +14,7 @@ export default function RegisterPage() {
 
   const handleRegister = async () => {
     try {
-      const res = await fetch("http://localhost:4000/api/users/signUp", {
+      const res = await fetch("https://charging-station-backend-o9ky.onrender.com/api/users/signUp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
