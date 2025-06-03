65dde2fd58f65d44de17df5221943abd876094a5
Suyash Pathak <suyashp271@gmail.com>
2025-06-03 14:59:35 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { router } from "./Routes/Auth.routes.js";
 import routerC from "./Routes/Charger.routes.js";
-const app = express()
+
+const app = express();
 
 app.use(cors({
   origin: 'https://charging-station-full-stack.vercel.app',
@@ -11,16 +12,22 @@ app.use(cors({
   credentials: true
 }));
 
+app.options('*', cors({
+  origin: 'https://charging-station-full-stack.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
-
-
 app.use(cookieParser());
 
 app.use('/api/users', router);
-app.use('/api/chargers',routerC);
-app.get('/api/users',(req,res)=>[
-    res.send("Welcome to api ")
-])
-export {app}
+app.use('/api/chargers', routerC);
+
+app.get('/api/users', (req, res) => {
+  res.send("Welcome to API");
+});
+
+export { app };
+
