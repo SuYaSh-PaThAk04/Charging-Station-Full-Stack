7d6215f2b4dd09f45e623af73f7039d9b6b039fb
Suyash Pathak <suyashp271@gmail.com>
2025-06-03 17:52:53 +0530

    Update Auth.routes.js

--- server/Routes/Auth.routes.js
+++ server/Routes/Auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import  VerifyJWT  from "../Middlewares/verifyJWT.middleware.js"
-import { changeCurrentPaasword, loginUser, signUpUser, updateAccountDetails } from "../Controller/Auth.Controllers.js"
+import { changeCurrentPaasword, loginUser, signUpUser, updateAccountDetails,logoutUser } from "../Controller/Auth.Controllers.js"
 
 
 const router = express.Router()
@@ -8,7 +8,7 @@ const router = express.Router()
 router.route('/signUp').post(signUpUser);
 
 router.route('/login').post(loginUser);
-router.route('/logout').post(VerifyJWT,loginUser)
+router.route('/logout').post(VerifyJWT,logoutUser)
 router.route('/chnagePass').post(changeCurrentPaasword)
 router.route('/UpdateDetails').post(updateAccountDetails)
 export {router}
