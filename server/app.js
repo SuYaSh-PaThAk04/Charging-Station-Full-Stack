7ab6c70033c392eda8359c0584fb36db7dd31260
Suyash Pathak <suyashp271@gmail.com>
2025-06-04 17:17:21 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.use(cors({
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+        console.error("Blocked by CORS:", origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
